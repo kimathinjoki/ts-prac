aaad425de03657e11856080acb5b3332dc7826bc
kimathinjoki <dfkimathinjoki@gmail.com>
2023-05-06 01:02:12 -0700

    typescript Tutorial

---
+++ sandbox.js
@@ -0,0 +1,52 @@
+// const char = "Mimi kwa mimi, sisi kwa sisi"
+// console.log(char)
+// const inputs = document.querySelectorAll('input')
+// console.log(inputs)
+// inputs.forEach((i)=> console.log(i))
+var char = "super mario";
+// let age = 30;
+var isBlackBelt = false;
+char = "dave";
+isBlackBelt = true;
+var circ = function (dia) {
+    return dia * Math.PI;
+};
+console.log(circ(7.9));
+// arrays
+var names = ["kim", "athi", "Njoki"];
+names.push("felo");
+// names.push(4)
+var mixed = ["ken", 67, 'chaun', 7, 9];
+mixed.push('tyu');
+mixed.push(78);
+mixed[0] = 9;
+//  objects 
+// let ninja = {
+//     name: "Ninja",
+//     age: 30,
+//     height: 1.7,
+//     belt: "white"
+// }
+// ninja.age = 40
+// ninja.belt = "black"
+// explicit types 
+var charas;
+var age;
+var isLoggedIn;
+age = 78;
+charas = "minions";
+// arrays
+var ninjas = [];
+ninjas.push("bradd");
+// union types
+var mixes = [];
+mixes.push("hello");
+mixes.push(89);
+mixes.push(true);
+var uid;
+uid = 89;
+uid = "34";
+// objects
+var ninjaOne;
+ninjaOne = { name: "yoshima", age: 89 };
+var ninjaTwo;
